118b4ce47a67494a39ec7c73eaf2f4473c9a57d5
cassiehenning <cjh281@georgetown.edu>
2021-05-16 20:30:33 -0400

    format email

--- app/MLB.py
+++ app/MLB.py
@@ -1,3 +1,4 @@
+from numpy.core.numeric import False_
 import statsapi
 import pandas as pd
 import numpy as np
@@ -21,31 +22,54 @@ print("For reference, the codes for Leagues are as follows...")
 print("American League is 103")
 print("National League is 104")
 print("------------------------------")
-print("The codes for Divisions are as follows...")
-print("American League West is 200")
-print("American League East is 201")
-print("American League Central is 202")
-print("National League West 203")
-print("National League East 204")
-print("National League Central 205")
-print("------------------------------")
 
 if APP_ENV == "development":
     lea = input("Please input a League code ")
-    div = input("Please input a Division code ")
+    if lea == "103":
+        print("The codes for Divisions are as follows...")
+        print("American League West is 200")
+        print("American League East is 201")
+        print("American League Central is 202")
+        print("------------------------------")
+        div = input("Please input a Division code for the American League ")
+    else:
+        print("The codes for Divisions are as follows...")
+        print("National League West 203")
+        print("National League East 204")
+        print("National League Central 205")
+        print("------------------------------")
+        div = input("Please input a Division code for the National League ")
     lea = int(lea)
     div = int(div)
 else:
     Lea = 104
     div = 204
 
+if div == 200:
+    division = "American League West"
+else:
+    if div == 201:
+        division = "American League East"
+    else:
+        if div == 202:
+            division = "American League Central"
+        else:
+            if div == 203:
+                division = "National League West"
+            else:
+                if div == 204:
+                    division = "National League East"
+                else:
+                    division = "National League Central"
+
 def get_division_standings(league,division):
     standings = pd.DataFrame(statsapi.standings_data(league)[division]['teams'])
     standings = standings[['name','w','l']]
     df = pd.DataFrame(statsapi.standings_data(league)[division]['teams'])
-    format_div_standings = df.to_html()
+    format_div_standings = df.to_html(index=False)
+    return format_div_standings
     print(format_div_standings)
-    return standings
+
 get_division_standings(lea,div) 
 
 def get_player_ids(name):
@@ -73,8 +97,9 @@ def get_stats_from_ids(name, stat_type):
         except: 
             pass
     dfr = pd.DataFrame(player_stats_list)  
-    format_stats = dfr.to_html()    
-    print(format_stats)  
+    format_stats = dfr.to_html(index=False)    
+    return(format_stats)
+    print(format_stats)
 get_stats_from_ids(player,'hitting')
 
 if __name__ == "__main__":
@@ -85,9 +110,9 @@ if __name__ == "__main__":
     <h2 style="color: darkslateblue;">Hi, here are your MLB updates!</h3>
     <h4 style="color: grey;">Today's Date</h4>
     <p style="color: grey;">{date.today().strftime('%A, %B %d, %Y')}</p>
-    <h2 style="color: darkslateblue;">Standings</h4>
+    <h2 style="color: darkslateblue;">Standings for {division} </h4>
         <p>{get_division_standings(lea,div)}</p>
-    <h2 style="color: darkslateblue;">Hitting Stats</h4>
+    <h2 style="color: darkslateblue;">Hitting Stats for {player} </h4>
         <p>{get_stats_from_ids(player,'hitting')}</p>
     <h2 style="color: darkslateblue;">Have a good day!</h2>
     """
